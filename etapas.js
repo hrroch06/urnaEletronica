3e8b84c4b92a0de097823a2c983570897684f5bd
Henrique Rocha <74513131+hrroch06@users.noreply.github.com>
2021-10-20 19:02:52 -0300

    Um Treinamento
    Projeto para treinar conhecimentos em JavaScript HTML CSS.

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url: '38111.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url: '77222.png', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Selivrando Mendonça',
+                partido: 'ABC',
+                vice: 'Metamao Lopes',
+                fotos:[
+                    {url: '99.png', legenda: 'Prefeito'},
+                    {url: '99_2.png', legenda: 'Vice-Prefeito', small:true}
+                ]
+
+            },
+            {
+                numero: '84',
+                nome: 'Zulano Malfalado',
+                partido: 'QWERTY',
+                vice: 'Falsilane Amarrado',
+                fotos:[
+                    {url: '84.png', legenda: 'Prefeito'},
+                    {url: '84_2.png', legenda: 'Vice-Prefeito', small:true}
+                    ]
+            },
+        ]
+    }
+]
